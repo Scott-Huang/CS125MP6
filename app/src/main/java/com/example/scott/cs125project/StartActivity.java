936e9fd4f625d6b6debe151132e1ee374949c376
Mingwei Huang <mingwei6@illinois.edu>
2018-11-17 14:02:52 -0600

    Initial commit

---
+++ app/src/main/java/com/example/scott/cs125project/StartActivity.java
@@ -0,0 +1,49 @@
+package com.example.scott.cs125project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button startBtn = (Button) findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent main = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(main);
+            }
+        });
+        Button loadBtn = (Button) findViewById(R.id.loadBtn);
+        loadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent load = new Intent(getApplicationContext(), LoadActivity.class);
+                startActivity(load);
+            }
+        });
+        Button settingBtn = (Button) findViewById(R.id.settingBtn);
+        settingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent setting = new Intent(getApplicationContext(), SettingActivity.class);
+                startActivity(setting);
+            }
+        });
+        Button exitBtn = (Button) findViewById(R.id.exitBtn);
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+    }
+}
