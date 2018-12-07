bc4b844ddd0199e2f23f4971d3b681c6c89c7d21
Mingwei Huang <mingwei6@illinois.edu>
2018-12-07 14:59:04 -0600

    finish the alpha version now

--- app/src/main/java/com/example/scott/cs125project/MainActivity.java
+++ app/src/main/java/com/example/scott/cs125project/MainActivity.java
@@ -9,7 +9,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import com.dragankrstic.autotypetextview.AutoTypeTextView;
 import java.util.ArrayList;
